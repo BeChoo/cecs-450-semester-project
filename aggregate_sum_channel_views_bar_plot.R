b5b898a8fc71c3e2850b0ca95a5c1b13b9050a3c
BeChoo <bcho2014@gmail.com>
2023-11-22 17:55:45 -0800

    Updated so that the plot takes the aggregate sum of views for each channel and plotted onto a bar plot. Every fifth label is shown.

--- aggregate_sum_channel_views_bar_plot.R
+++ aggregate_sum_channel_views_bar_plot.R
@@ -3,6 +3,7 @@ library(ggplot2)
 library(dplyr)
 library(scales)
 library(tidyverse)
+library(stringr)
 
 # Gets the current directory and adds the "csv" folder to the path
 # This assumes that the path is set up the same way as my local machine
@@ -29,19 +30,26 @@ for (file in files) {
   
   # 'views' is being read as a string, this will update it to an int
   data <- data %>%
-    mutate(views = as.numeric(gsub(",", "", views)))
+    mutate(numeric_views = as.numeric(gsub(",", "", views)))
   
   # Calculate the sum of views
-  sum_views <- sum(data$views, na.rm = TRUE)
+  sum_views <- sum(data$numeric_views, na.rm = TRUE)
   
   # Append the data to the aggregate data frame
   aggregate_data <- rbind(aggregate_data, data.frame(file = file, sum_views = sum_views))
 }
 
+aggregate_data$file <- str_extract(aggregate_data$file, "_([^_]+)_")
+
 # Create a bar plot of the aggregated data
 ggplot(aggregate_data, aes(x = file, y = sum_views)) +
   geom_bar(stat = "identity") +
   labs(title = "Bar Plot of Aggregate Views", x = "Channel", y = "Sum of Views") +
-  theme_minimal()
+  # TODO
+  # scale_y_log10 isn't clear enough, try another scale
+  # scale_y_log10() +
+  scale_y_continuous(labels = comma) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 30, hjust = 1, vjust = 1))
 
 dev.off()
