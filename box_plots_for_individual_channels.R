687e895dda7942454f4dcdc842d10d4f2b918f02
BeChoo <bcho2014@gmail.com>
2023-11-27 23:38:42 -0800

    Updated file

--- box_plots_for_individual_channels.R
+++ box_plots_for_individual_channels.R
@@ -1,2 +1,49 @@
 library(tidyverse)
 
+# Gets the current directory
+working_dir <- getwd()
+
+# Save the plot to a PDF in the "graph_pics" folder
+pdf("graph_pics/Box_Plots.pdf")
+
+# Navigate to the CSV files and add the "csv" folder to the path
+directory_path <- paste(working_dir, "/csv", sep = '')
+
+# Set the working directory to the directory path set above
+setwd(directory_path)
+
+# List all files in the directory, ignoring files that are not CSVs
+files <- list.files(directory_path, pattern = "\\.csv$", full.names = TRUE)
+
+# Create an empty data frame to store aggregated data
+aggregate_data <- data.frame()
+
+# Create an empty list to store plots
+plots <- list()
+
+# Iterate through each file
+for (file in files) {
+  # Read the CSV file
+  data <- read.csv(file, sep = '~')
+  
+  # 'views' is being read as a string, this will update it to an int
+  data <- data %>%
+    mutate(numeric_views = as.numeric(gsub(",", "", views)))
+  
+  # Create a box plot for the current file
+  plot <- ggplot(data, aes(x = file, y = numeric_views)) +
+    geom_boxplot() +
+    labs(title = paste("Box Plot of Views for", file), x = "Channel", y = "Numeric Views") +
+    scale_y_continuous(labels = scales::comma) +
+    theme_minimal() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1, size = 7))
+  
+  # Save the plot to the list
+  plots[[file]] <- plot
+}
+
+for (plot in plots) {
+  print(plot)
+}
+
+dev.off()
